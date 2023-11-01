acaf47928cf133b04e7bf6117d043e355d1b2adf
Sean Trim <44306317+seantrim@users.noreply.github.com>
2023-11-01 16:58:37 -0600

    Updates to Fortran files

---
+++ Fortran/compile_script.sh
@@ -0,0 +1,18 @@
+# Select driver program
+DRIVER_FILE="ellipFor_test_driver.f90" #options are "ellipFor_test_driver.f90" "test_material_driver.f90"
+
+# ----------------- No User Input Required Past This Point --------------------
+
+# source files containing algorithm implementation and the driver
+SOURCE_FILES="elliptic.f90 xelbdj2_all_routines.f90 xgscd_routines.f90 ${DRIVER_FILE}"
+
+# determine object file names
+OBJECT_FILES=( $(basename -s .f90 $(basename -a $SOURCE_FILES)) )
+OBJECT_FILES="${OBJECT_FILES[@]/%/.o}" 
+
+#Optimized for speed
+gfortran -flto -O3 -c $SOURCE_FILES #complile object files
+gfortran -flto -O3 $OBJECT_FILES -o $(basename -s .f90 ${DRIVER_FILE}) #link object files
+
+##Clean up object files
+rm -f *.o *.mod
---
+++ Fortran/ellipFor_test_driver.f90
@@ -0,0 +1,41 @@
+program ellipFor_test_driver
+! *** Driver program demonstrating the basic use of ellipFor featuring the evaluation of 1) incomplete Legendre elliptic integrals, 2) complete Legendre elliptic integrals, and 3) the primary Jacobi elliptic functions ***
+ use kind_parameters
+implicit none
+
+!!Useful parameters
+real(dp), parameter :: pii=3.1415926535897932d0
+
+!!variables for elliptic functions and integrals
+real(dp) :: m           !!elliptic parameter
+real(dp) :: phi         !!Jacobi amplitude
+complex(dp) :: u        !!complex argument
+complex(dp) :: Fc,Ec    !!complete elliptic integrals of first and second kinds
+complex(dp) :: Fi,Ei    !!incomplete elliptic integrals of first and second kinds 
+complex(dp) :: sn,cn,dn !!Jacobi elliptic function values
+
+open(unit=101,file="ellipFor_test_driver.dat") !! file for driver output
+
+!!! 1) complete Legendre elliptic integrals
+m=100.d0
+call complete_elliptic_integrals(m,Fc,Ec)
+write(101,*) "Complete Legendre Elliptic Integrals:"
+write(101,*) m,Fc,Ec
+
+!!! 2) incomplete Legendre elliptic integrals
+phi=0.75*pii 
+m=100.d0     
+call incomplete_elliptic_integrals(phi,m,Fi,Ei)
+write(101,*) "Incomplete Legendre Elliptic Integrals:"
+write(101,*) phi,m,Fi,Ei
+
+!!! 3) Jacobi elliptic functions
+u=(1.d0,1.d0) ! complex-valued u
+m=100.d0      
+call Jacobi_elliptic_functions(u,m,sn,cn,dn)
+write(101,*) "Jacobi Elliptic Functions:"
+write(101,*) u,m,sn,cn,dn
+
+close(101) !! close output file
+end program ellipFor_test_driver
+
