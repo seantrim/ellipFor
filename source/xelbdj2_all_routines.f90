804907de6bc20f0f2e3f2e55a41b82955cc0fb26
seantrim <strim201325@gmail.com>
2024-12-12 04:37:08 -0600

    Portability updates for xelbdj2_all_routines module: celbdj routine.

--- source/xelbdj2_all_routines.f90
+++ source/xelbdj2_all_routines.f90
@@ -612,15 +612,15 @@ contains
  real(dp),intent(out) :: celb,celd,celj !!SJT
  logical flag
  integer(isp) IMAX,i,is,ie
- parameter (IMAX=40)
- real(dp) y(0:IMAX),x(0:IMAX),c(0:IMAX),d(0:IMAX),a(0:IMAX)
+ parameter (IMAX=40_isp)
+ real(dp) y(0_isp:IMAX),x(0_isp:IMAX),c(0_isp:IMAX),d(0_isp:IMAX),a(0_isp:IMAX)
  real(dp) mc,mc0,nc,m,n,celk,yi,ye,dj,m1,kc0,temp !!SJT
  real(qp),intent(in) :: mc0_qp !!SJT
  real(qp) :: mc_qp !!SJT
  real(dp) PIHALF,EPS,THIRD
- parameter (PIHALF=1.5707963267948966d0)
- parameter (EPS=1.11d-16)
- parameter (THIRD=1.d0/3.d0)
+ parameter (PIHALF=1.5707963267948966e0_dp)
+ parameter (EPS=1.11e-16_dp)
+ parameter (THIRD=1.e0_dp/3.e0_dp)
  real(dp) B(IMAX)
  !logical first /.TRUE./  !!!SJT: note that logical variable first is .true. at the start of each routine call
  !save B,first !!SJT: disable for thread safety
@@ -635,22 +635,22 @@ contains
  !    write(*,*) "(celbdj) Out of domain: nc <= 0"
  !    return
  !endif
- if(mc0.lt.1.d0) then
+ if(mc0.lt.1.e0_dp) then
      !mc=mc0;nc=nc0
      mc_qp=mc0_qp; nc=nc0 !!SJT
- elseif(mc0.gt.1.d0) then
-     !mc=1.d0/mc0;nc=nc0*mc !!SJT: OG
+ elseif(mc0.gt.1.e0_dp) then
+     !mc=1.e0_dp/mc0;nc=nc0*mc !!SJT: OG
      mc_qp=1.0_qp/mc0_qp; nc=nc0*real(mc_qp,dp) !!SJT
  else
-     celb=PIHALF*0.5d0
-     celd=PIHALF*0.5d0
+     celb=PIHALF*0.5e0_dp
+     celd=PIHALF*0.5e0_dp
      celj=PIHALF/(nc0+sqrt(nc0))
      return
  endif
  if(first) then
      first=.FALSE.
-     do i=1,IMAX
-         B(i)=1.d0/dble(2*i+1)
+     do i=1_isp,IMAX
+         B(i)=1.e0_dp/dble(2_isp*i+1_isp)
      enddo
  endif
  mc=real(mc_qp,dp) !!SJT
@@ -661,57 +661,57 @@ contains
      goto 4  
  endif
  !m=1.d0-mc; n=1.d0-nc !!SJT: original
- m=real(1.0_qp-mc_qp,dp); n=1.d0-nc !!SJT
+ m=real(1.0_qp-mc_qp,dp); n=1.e0_dp-nc !!SJT
  flag=nc.lt.mc.or.(n*nc).gt.(nc-mc)
  if(flag) then
-     y(0)=(nc-mc)/(nc*m)
+     y(0_isp)=(nc-mc)/(nc*m)
  else
-     y(0)=n/m
+     y(0_isp)=n/m
  endif
- is=0
- if(y(0).gt.0.5d0) then
-     x(0)=1.d0-y(0)
-     do i=0,IMAX-1 !!SJT
+ is=0_isp
+ if(y(0_isp).gt.0.5d0) then
+     x(0_isp)=1.e0_dp-y(0_isp)
+     do i=0_isp,IMAX-1_isp !!SJT
      !do i=0,IMAX !!SJT: OG
          c(i)=sqrt(x(i))
          d(i)=sqrt(mc+m*x(i))
-         x(i+1)=(c(i)+d(i))/(1.d0+d(i))
-         if(x(i+1).gt.0.5d0) then
-             y(i+1)=y(i)/((1.d0+c(i))*(1.d0+d(i)))
-             is=i+1
+         x(i+1_isp)=(c(i)+d(i))/(1.e0_dp+d(i))
+         if(x(i+1_isp).gt.0.5e0_dp) then
+             y(i+1_isp)=y(i)/((1.e0_dp+c(i))*(1.e0_dp+d(i)))
+             is=i+1_isp
              goto 1
          endif
-         y(i+1)=1.d0-x(i+1)
+         y(i+1_isp)=1.e0_dp-x(i+1_isp)
      enddo
      !write(*,"(a30,i5)") "(celbdj) No Conv. x-Transf. i=",i !!SJT: does not occur in practice
      return
  endif
  1 continue
- do i=is,IMAX-1 !!SJT
+ do i=is,IMAX-1_isp !!SJT
  !do i=is,IMAX !!SJT: OG
-     c(i)=sqrt(1.d0-y(i))
-     d(i)=sqrt(1.d0-m*y(i))
-     y(i+1)=y(i)/((1.d0+c(i))*(1.d0+d(i)))
-     if(abs(y(i+1)).lt.0.325d0) goto 2
+     c(i)=sqrt(1.e0_dp-y(i))
+     d(i)=sqrt(1.e0_dp-m*y(i))
+     y(i+1_isp)=y(i)/((1.e0_dp+c(i))*(1.e0_dp+d(i)))
+     if(abs(y(i+1_isp)).lt.0.325e0_dp) goto 2
  enddo
  !write(*,"(a30,i5)") "(celbdj) No Conv. y-Transf. i=",i !!SJT: does not occur in practice
  return
  2 continue
- ie=i+1
+ ie=i+1_isp
  ye=y(ie)
  celk=celb+celd
- a(0)=celd
- celj=a(0)
+ a(0_isp)=celd
+ celj=a(0_isp)
  yi=ye
- a(1)=((1.d0+2.d0*m)*celd-celb)*THIRD
- dj=a(1)*yi
- i=1
+ a(1_isp)=((1.e0_dp+2.e0_dp*m)*celd-celb)*THIRD
+ dj=a(1_isp)*yi
+ i=1_isp
  if(abs(dj).lt.EPS*abs(celj)) goto 3
  celj=celj+dj
- m1=1.d0+m
- do i=2,IMAX
+ m1=1.e0_dp+m
+ do i=2_isp,IMAX
      yi=yi*ye
-     a(i)=(1.d0-B(i))*m1*a(i-1)-(1.d0-2.d0*B(i))*m*a(i-2)
+     a(i)=(1.e0_dp-B(i))*m1*a(i-1_isp)-(1.e0_dp-2.e0_dp*B(i))*m*a(i-2_isp)
      dj=a(i)*yi
      if(abs(dj).lt.EPS*abs(celj)) goto 3
      celj=celj+dj
@@ -719,14 +719,14 @@ contains
  !write(*,"(a30,i5)") "(celbdj) No Conv. Series. i=",i !!SJT: does not occur in practice
  return
  3 continue
- do i=ie-1,0,-1
-     celj=(2.d0*(c(i)+d(i))*celj-y(i)*celk)/(c(i)*d(i)*(1.d0+c(i))*(1.d0+d(i)))
+ do i=ie-1_isp,0_isp,-1_isp
+     celj=(2.e0_dp*(c(i)+d(i))*celj-y(i)*celk)/(c(i)*d(i)*(1.e0_dp+c(i))*(1.e0_dp+d(i)))
  enddo
  if(flag) then
      celj=(nc*celk-mc*celj)/(nc*nc)
  endif
  4 continue
- if(mc0.gt.1.d0) then
+ if(mc0.gt.1.e0_dp) then
      kc0=sqrt(mc0)
      temp=celb
      celb=celd/kc0
